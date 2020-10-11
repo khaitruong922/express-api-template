330700a083927822b83721ed5b216c58bafac35a
Chung Quan Tin <cqtin0903@gmail.com>
2020-10-11 15:52:06 +0700

    configure project source tree

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
+const bodyParser = require("body-parser"),
+	cookieParser = require("cookie-parser");
+
+const express = require("express");
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(process.env.NODE_SERVER_PORT, () => {
+	console.log(`✔ Our server is running on port ${process.env.PORT}`);
+});

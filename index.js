2c84c869c3380e2a8f44cf0419dd4c0d5b654bf2
Khai <kd.kidr@gmail.com>
2020-10-12 18:22:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,28 @@ if (process.env.NODE_ENV !== "production") {
 const { mongoose, db } = require("./utils/mongoose");
 const MongoDBConfig = require("./configs/mongodb.config");
 // EXPRESS
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
+// CONST
+const PORT = process.env.PORT || 3000
+const MONGO_URI = process.env.MONGO_URI
 // SERVER UTILITIES
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 // INITIALIZE MONGO CONNECTION
-mongoose.connect(process.env.MONGO_URI, MongoDBConfig, () => {
-   console.log("✔ MongoDB database is connection");
-});
+mongoose.connect(MONGO_URI, MongoDBConfig, () => {
+   console.log("✔ MongoDB database is connection")
+})
 
-db.on("error", console.error.bind(console, "❌ Connection error: "));
+db.on("error", console.error.bind(console, "❌ Connection error: "))
 db.on(
    "ready",
    console.error.bind(console, "✔ Database connected successfully ")
-);
-app.listen(process.env.NODE_SERVER_PORT, () => {
-   console.log(`✔ Our server is running on port ${process.env.PORT}`);
-});
+)
+app.listen(PORT, () => {
+   console.log(`✔ Our server is running on port ${PORT}`)
+})

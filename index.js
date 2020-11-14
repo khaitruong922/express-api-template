a9d954272e3f60b67f2495056c29ee04b43d853c
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2020-11-14 12:11:28 +0700

    fix api route

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") {
-   require("dotenv").config();
+    require("dotenv").config();
 }
 // DATABASE SETUP
 const { mongoose, db } = require("./utils/mongoose");
@@ -7,41 +7,41 @@ const MongoDBConfig = require("./configs/mongodb.config");
 // EXPRESS
 const express = require("express")
 const app = express()
-// CONST
+    // CONST
 const PORT = process.env.PORT || 3001
 const MONGO_URI = process.env.MONGO_URI
-// SERVER UTILITIES
+    // SERVER UTILITIES
 const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser")
-// ROUTERS
+    // ROUTERS
 const ApiRoute = require('./routes/api')
 const AuthRoute = require('./routes/auth')
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
-// INITIALIZE MONGO CONNECTION
+    // INITIALIZE MONGO CONNECTION
 mongoose.connect(MONGO_URI, MongoDBConfig, () => {
-   console.log("✔ MongoDB database is connected")
+    console.log("✔ MongoDB database is connected")
 })
 
 db.on("error", console.error.bind(console, "❌ Connection error: "))
 app.listen(PORT, () => {
-   console.log(`✔ Our server is running on http://localhost:${PORT}`)
+    console.log(`✔ Our server is running on http://localhost:${PORT}`)
 })
 
 // ROUTES
-app.use('/api/v1', ApiRoute)
+app.use('/api', ApiRoute)
 app.use('/', AuthRoute)
 
 // ERROR HANDLING MIDDLEWARE
 app.use((err, req, res, next) => {
-   res.status(422).send({
-      error: err.message
-   })
+    res.status(422).send({
+        error: err.message
+    })
 })
 
 // DEFAULT VIEW
 app.get("/", (req, res) => {
-   res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/public/index.html')
 })
